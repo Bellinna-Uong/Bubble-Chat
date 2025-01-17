937563bfaece742bb9b8a46dfc15d03b639307e9
belli <bellinna.uong@outlook.com>
2025-01-17 15:21:37 +0100

    cleaning file + add windowchat.jsx

--- main/src/app/chat.jsx
+++ main/src/app/chat.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import '../style.css'
 
-function Chat() {
+const Chat = () => {
     return (
         <div>
             <h1>Bienvenue dans le chat</h1>
+            <p>Vous pouvez maintenant discuter avec vos amis!</p>
         </div>
     );
-}
+};
 
 export default Chat;
---
+++ main/src/connexion/connexion-main.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import SignInForm from './signin';
+import SignUpForm from './signup';
+import Chat from '../app/chat';
+
+
+const App = () => {
+    const [isSignIn, setIsSignIn] = useState(true);
+    const [isChatVisible, setIsChatVisible] = useState(false);
+
+    const switchToSignUp = () => setIsSignIn(false);
+    const switchToSignIn = () => setIsSignIn(true);
+    const navigateToChat = () => setIsChatVisible(true);
+
+    return (
+        <div>
+            {isChatVisible ? (
+                <Chat />
+            ) : isSignIn ? (
+                <SignInForm switchToSignUp={switchToSignUp} navigateToChat={navigateToChat} />
+            ) : (
+                <SignUpForm switchToSignIn={switchToSignIn} />
+            )}
+        </div>
+    );
+};
+
+export default App;

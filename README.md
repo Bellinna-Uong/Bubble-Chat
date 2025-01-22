457393288c13955f1de4186db7885e20abf73337
belli <bellinna.uong@outlook.com>
2025-01-22 22:11:00 +0100

    clean + add png

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Bubble-Chat
+
+# How to run ?
+- backend : being in root and : php -S localhost:8000 -t backend
+- front : being in frontend : npm run dev
--- backend/data.sql
+++ backend/data.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS chat_app;
 USE chat_app;
 
 CREATE TABLE IF NOT EXISTS users (
-                                     id INT AUTO_INCREMENT PRIMARY KEY,
-                                     username VARCHAR(100) NOT NULL UNIQUE,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
     );
